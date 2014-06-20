d5135a492cce5b6910d924098e8db87321ab1867
Anton Nuzhny <anuzhny@localhost.localdomain>
2014-06-20 12:06:13 +0400

    commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,14 @@
 #Step 1: "Merges the training and the test sets to create one data set."
 # read subject training data
-subject_train = read.table("UCI HAR Dataset/train/subject_train.txt", col.names=c("subject_id"))
+subject_train = read.table("UCIHARDataset/train/subject_train.txt", col.names=c("subject_id"))
 # assign row number as the values of ID column
 subject_train$ID <- as.numeric(rownames(subject_train))
 # read training data
-X_train = read.table("UCI HAR Dataset/train/X_train.txt")
+X_train = read.table("UCIHARDataset/train/X_train.txt")
 # assign row number as the values of ID column
 X_train$ID <- as.numeric(rownames(X_train))
 # read activity training data
-y_train = read.table("UCI HAR Dataset/train/y_train.txt", col.names=c("activity_id"))  # max = 6
+y_train = read.table("UCIHARDataset/train/y_train.txt", col.names=c("activity_id"))  # max = 6
 #y_train = merge(y_train, activity_labels)
 # assign row number as the values of ID column
 y_train$ID <- as.numeric(rownames(y_train))
@@ -18,15 +18,15 @@ train <- merge(subject_train, y_train, all=TRUE)
 train <- merge(train, X_train, all=TRUE)
 
 # read subject training data
-subject_test = read.table("UCI HAR Dataset/test/subject_test.txt", col.names=c("subject_id"))
+subject_test = read.table("UCIHARDataset/test/subject_test.txt", col.names=c("subject_id"))
 # assign row number as the values of ID column
 subject_test$ID <- as.numeric(rownames(subject_test))
 # read testing data
-X_test = read.table("UCI HAR Dataset/test/X_test.txt")
+X_test = read.table("UCIHARDataset/test/X_test.txt")
 # assign row number as the values of ID column
 X_test$ID <- as.numeric(rownames(X_test))
 # read activity testing data
-y_test = read.table("UCI HAR Dataset/test/y_test.txt", col.names=c("activity_id"))  # max = 6
+y_test = read.table("UCIHARDataset/test/y_test.txt", col.names=c("activity_id"))  # max = 6
 #y_test = merge(y_test, activity_labels)
 # assign row number as the values of ID column
 y_test$ID <- as.numeric(rownames(y_test))
@@ -39,13 +39,13 @@ test <- merge(test, X_test, all=TRUE)
 data1 <- rbind(train, test)
 
 #Step 2: "Extracts only the measurements on the mean and standard deviation for each measurement."
-features = read.table("UCI HAR Dataset/features.txt", col.names=c("feature_id", "feature_label"),)  #561
+features = read.table("UCIHARDataset/features.txt", col.names=c("feature_id", "feature_label"),)  #561
 #Extracts only the measurements on the mean and standard deviation for each measurement. 
 selected_features <- features[grepl("mean\\(\\)", features$feature_label) | grepl("std\\(\\)", features$feature_label), ]
 data2 <- data1[, c(c(1, 2, 3), selected_features$feature_id + 3) ]
 
 #Step 3: "Uses descriptive activity names to name the activities in the data set."
-activity_labels = read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("activity_id", "activity_label"),) #
+activity_labels = read.table("UCIHARDataset/activity_labels.txt", col.names=c("activity_id", "activity_label"),) #
 data3 = merge(data2, activity_labels)
 
 #Step 4: "Appropriately labels the data set with descriptive activity names." 
